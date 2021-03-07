7cf4cdb69bb9f9d10ce77594f9e3c5991775974e
Jesse Wenger <wengerj@gmail.com>
2021-03-07 22:49:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,8 +39,13 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    self.find_by_name(name)
-    self.create_by_name(name)
+    song = self.find_by_name(name)
+    
+    if song.name == name
+      song
+    else
+      self.create_by_name(name)
+    end
     
 =begin
     song_found = self.find_by_name(name)
