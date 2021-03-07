2b4d49a4a65bf937a76d9dc6239df474e30042c1
Jesse Wenger <wengerj@gmail.com>
2021-03-07 21:08:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,9 +12,6 @@ class Song
   end
   
   def self.create
-    #song = self.new
-    #self.all << song
-    #song
     self.all << self.new
   end
   
@@ -25,9 +22,7 @@ class Song
   end
   
   def self.create_by_name(name)
-    song = self.new_by_name(name)
-    self.all << song
-    song
+    self.all << self.new_by_name(name)
   end
   
   def self.find_by_name(name)
